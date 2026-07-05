4291f24148b01c51983e83b35e84d373a377165b
akankit354656 <aksheoran1024@gmail.com>
2026-07-05 17:58:47 +0530

    Add initial UI and expense popup

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("JS executed successfully");
+const container = document.querySelector(".items");
+const add_item_btn = document.getElementById("add_item_button");
+const popUp = document.querySelector(".pop_up")
+const cnclPopup = document.querySelector(".cancel_popup_button");
+
+add_item_btn.addEventListener("click", ()=>{
+    popUp.classList.add("show");
+})
+cnclPopup.addEventListener("click", ()=>{
+    popUp.classList.remove("show");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
