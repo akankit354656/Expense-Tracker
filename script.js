1e8dd0cbc765bce73a7f255b0e3b1020af1e62b3
akankit354656 <aksheoran1024@gmail.com>
2026-07-06 15:33:17 +0530

    Pop up and form raw updation

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const container = document.querySelector(".items");
 const add_item_btn = document.getElementById("add_item_button");
 const popUp = document.querySelector(".pop_up")
 const cnclPopup = document.querySelector(".cancel_popup_button");
+const submit_button = document.getElementById("submit");
+
+function add_item_fun(){
+    // const name = document.getElementById("title");
+    // const category = document.getElementById("category");
+    // const price = document.getElementById("amount");
+    // const expense_row = document.createElement("div");
+    // const Name = document.createElement("div");
+    // const Category = document.createElement("div");
+    // const Price = document.createElement("div");
+    // const delete_btn = document.createElement("button");
+    // const edit_btn = document.createElement("button");
+    // Name.textContent = name.value();
+    console.log("Submitted succesfully..");
+
+}
+
 
 add_item_btn.addEventListener("click", ()=>{
     popUp.classList.add("show");
@@ -9,8 +26,9 @@ add_item_btn.addEventListener("click", ()=>{
 cnclPopup.addEventListener("click", ()=>{
     popUp.classList.remove("show");
 })
-
-
+submit_button.addEventListener("click", ()=>{
+    add_item_fun();
+})
 
 
 
