0b69fb03e6c0274b06795393be7d4e9ba14ea76f
akankit354656 <aksheoran1024@gmail.com>
2026-07-07 14:07:59 +0530

    Display of expenses and popup updated

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
+console.log("js started");
 const container = document.querySelector(".items");
 const add_item_btn = document.getElementById("add_item_button");
 const popUp = document.querySelector(".pop_up")
 const cnclPopup = document.querySelector(".cancel_popup_button");
 const submit_button = document.getElementById("submit");
+const expenseListDiv = document.getElementById("expenseListDiv");
+const expenseList = [];
 
-function add_item_fun(){
-    // const name = document.getElementById("title");
-    // const category = document.getElementById("category");
-    // const price = document.getElementById("amount");
-    // const expense_row = document.createElement("div");
-    // const Name = document.createElement("div");
-    // const Category = document.createElement("div");
-    // const Price = document.createElement("div");
-    // const delete_btn = document.createElement("button");
-    // const edit_btn = document.createElement("button");
-    // Name.textContent = name.value();
-    console.log("Submitted succesfully..");
-
+function displayExpense(expense){
+    const row = document.createElement("div");
+    row.className = "expense";
+    row.innerHTML = `
+    <span>${expense.expense_name}</span>
+    <span>${expense.expense_category}</span>
+    <span>${expense.expense_amount}</span>
+    <button>Delete</button>
+    <button>Edit</button>
+    `;
+    expenseListDiv.appendChild(row);
 }
 
+function add_item_fun(){
+
+    const name = document.getElementById("title").value;
+    const amount = document.getElementById("amount").value;
+    const category = document.getElementById("category").value;
+    const expense = {
+        expense_name : name,
+        expense_amount : amount,
+        expense_category : category
+    };
+    expenseList.push(expense);
+    localStorage.setItem(
+        "expenses",
+        JSON.stringify(expenseList)
+    );
+    displayExpense(expense);
+
+}
 
 add_item_btn.addEventListener("click", ()=>{
     popUp.classList.add("show");
@@ -28,18 +47,6 @@ cnclPopup.addEventListener("click", ()=>{
 })
 submit_button.addEventListener("click", ()=>{
     add_item_fun();
+    popUp.classList.remove("show");
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
 
