32494598d1574d6ce618eb61cad7c98ea50aacdd
akankit354656 <aksheoran1024@gmail.com>
2026-07-09 22:15:51 +0530

    Local storage added

--- script.js
+++ script.js
@@ -5,26 +5,38 @@ const popUp = document.querySelector(".pop_up")
 const cnclPopup = document.querySelector(".cancel_popup_button");
 const submit_button = document.getElementById("submit");
 const expenseListDiv = document.getElementById("expenseListDiv");
-const expenseList = [];
+// const expenseList = [];
+const expenseList = JSON.parse(localStorage.getItem("expenses")) || [];
+expenseList.forEach((expense, index) => {
+    displayExpense(expense, index);
+});
 
-function displayExpense(expense){
+function displayExpense(expense, index){
     const row = document.createElement("div");
     row.className = "expense";
     row.innerHTML = `
     <span>${expense.expense_name}</span>
     <span>${expense.expense_category}</span>
-    <span>${expense.expense_amount}</span>
-    <button>Delete</button>
-    <button>Edit</button>
+    <span>₹ ${expense.expense_amount}</span>
+    <button id=${index} >Delete</button>
+    <button >Edit</button>
     `;
     expenseListDiv.appendChild(row);
+    document.getElementById(`${index}`).addEventListener("click", ()=>{
+        console.log("Delete clicked", index);
+        expenseList.splice(index, 1);
+        localStorage.setItem(
+            "expenses",
+            JSON.stringify(expenseList)
+        );
+    });
 }
 
 function add_item_fun(){
 
-    const name = document.getElementById("title").value;
-    const amount = document.getElementById("amount").value;
-    const category = document.getElementById("category").value;
+    let name = document.getElementById("title").value;
+    let amount = document.getElementById("amount").value;
+    let category = document.getElementById("category").value;
     const expense = {
         expense_name : name,
         expense_amount : amount,
@@ -35,6 +47,7 @@ function add_item_fun(){
         "expenses",
         JSON.stringify(expenseList)
     );
+   
     displayExpense(expense);
 
 }
